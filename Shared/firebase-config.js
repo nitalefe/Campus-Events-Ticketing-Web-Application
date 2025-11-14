2b7ee3d9b76d69f9985887289d0fa09d0c8293bf
Ayan Hoque <166723783+YanKillah@users.noreply.github.com>
2025-11-13 21:16:11 -0500

    Delete Shared/firebase-config.js
    deleted

--- Shared/firebase-config.js
+++
@@ -1,3 +0,0 @@
-// Firebase configuration has been removed for security reasons.
-// Please provide your Firebase config via environment variables or a secure, non-committed file.
-// This file should not contain any credentials or sensitive information.
